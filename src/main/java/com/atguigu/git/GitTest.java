3c66cc0b86f334d8014e9f8b7e6800f6746d9be8
Booker <Booker@qq.com>
2021-07-08 20:55:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: booky
+ * @create: 2021-07-08 20:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
